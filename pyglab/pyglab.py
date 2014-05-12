4764b5248cf91042a12ce6aef77a04c37360eb4f
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-12 07:22:09 +0200

    Add initial shell of Pyglab class.

---
+++ pyglab/pyglab.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class Pyglab(object):
+    def __init__(self, token):
+        self.token = token
+        self.headers = {'PRIVATE-TOKEN', token}
+        self.user = None
+
+    def sudo(self, user):
+        """Permanently set a different username. Returns the old username."""
+
+        previous_user = self.user
+        self.user = user
+        return previous_user
