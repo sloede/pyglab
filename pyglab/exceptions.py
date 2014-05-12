4b0174cbac861fcc4360b31d6481111e2fd5a727
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-12 10:45:09 +0200

    Properly implement RequestError with metaclass.

--- pyglab/exceptions.py
+++ pyglab/exceptions.py
@@ -8,74 +8,58 @@ class RequestErrorMeta(type):
             cls.errors = {}
 
 class RequestError(Exception, metaclass=RequestErrorMeta):
-    def __init__(self, statuscode, message, body=None):
-        self.statuscode = statuscode
-        self.message = message
+    def __init__(self, body=None):
         self.body = body
 
     def __str__(self):
         return "{m} [status:{s}]".format(s=self.statuscode, m=self.message)
 
+    @classmethod
+    def is_error(cls, statuscode):
+        return statuscode in cls.errors
+
+    @classmethod
+    def get_error_class(cls, statuscode):
+        return cls.errors.get(statuscode, None)
+
 class BadRequestError(RequestError):
     statuscode = 400
     message = "A required attribute of the API request is missing."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(self.statuscode, self.message, body)
+        super(BadRequestError, self).__init__(body)
 
 class UnauthorizedError(RequestError):
     statuscode = 401
     message = "The user is not authenticated, a valid user token is necessary."
     def __init__(self, body=None):
-        super(UnauthorizedError, self).__init__(self.statuscode, self.message, body)
+        super(UnauthorizedError, self).__init__(body)
 
 class ForbiddenError(RequestError):
     statuscode = 403
     message = "The request is not allowed, user lacks necessary permissions."
     def __init__(self, body=None):
-        super(ForbiddenError, self).__init__(self.statuscode, self.message, body)
+        super(ForbiddenError, self).__init__(body)
 
 class NotFoundError(RequestError):
     statuscode = 404
     message = "A resource could not be accessed, i.e. it was not found."
     def __init__(self, body=None):
-        super(NotFoundError, self).__init__(self.statuscode, self.message, body)
+        super(NotFoundError, self).__init__(body)
 
 class MethodNotAllowedError(RequestError):
     statuscode = 405
     message = "The request is not supported by the API."
     def __init__(self, body=None):
-        super(MethodNotAllowedError, self).__init__(self.statuscode, self.message, body)
+        super(MethodNotAllowedError, self).__init__(body)
 
 class ConflictError(RequestError):
     statuscode = 409
     message = "A conflicting resource (i.e. with the same name) already exists."
     def __init__(self, body=None):
-        super(ConflictError, self).__init__(self.statuscode, self.message, body)
+        super(ConflictError, self).__init__(body)
 
 class ServerError(RequestError):
     statuscode = 500
     message = "While handling the request something went wrong on the server side."
     def __init__(self, body=None):
-        super(ServerError, self).__init__(self.statuscode, self.message, body)
-
-_errors = {
-        400: BadRequestError,
-        401: UnauthorizedError,
-        403: ForbiddenError,
-        404: NotFoundError,
-        405: MethodNotAllowedError,
-        409: ConflictError,
-        500: ServerError,
-        }
-
-def is_error(statuscode):
-    return statuscode in _errors
-
-def is_error2(statuscode):
-    return statuscode in RequestError.errors
-
-def get_error_class(statuscode):
-    return _errors[statuscode]
-
-def get_error_class2(statuscode):
-    return RequestError.errors[statuscode]
+        super(ServerError, self).__init__(body)
