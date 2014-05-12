07095d376b3213d0b55543c5d0b8ddecaf14a184
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-12 08:05:52 +0200

    Finish first implementation of exceptions.

--- pyglab/exceptions.py
+++ pyglab/exceptions.py
@@ -4,50 +4,57 @@ class RequestsError(Exception):
         self.message = message
         self.body = body
 
+    def __str__(self):
+        return "{m} [status:{s}]".format(s=self.statuscode, m=self.message)
+
 class BadRequestError(RequestsError):
     message = "A required attribute of the API request is missing."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(400, message, body)
+        super(BadRequestError, self).__init__(400, self.message, body)
 
 class UnauthorizedError(RequestsError):
     message = "The user is not authenticated, a valid user token is necessary."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(401, message, body)
+        super(UnauthorizedError, self).__init__(401, self.message, body)
 
 class ForbiddenError(RequestsError):
     message = "The request is not allowed, user lacks necessary permissions."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(403, message, body)
+        super(ForbiddenError, self).__init__(403, self.message, body)
 
 class NotFoundError(RequestsError):
     message = "A resource could not be accessed, i.e. it was not found."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(404, message, body)
+        super(NotFoundError, self).__init__(404, self.message, body)
 
 class MethodNotAllowedError(RequestsError):
     message = "The request is not supported by the API."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(405, message, body)
+        super(MethodNotAllowedError, self).__init__(405, self.message, body)
 
 class ConflictError(RequestsError):
     message = "A conflicting resource (i.e. with the same name) already exists."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(409, message, body)
+        super(ConflictError, self).__init__(409, self.message, body)
 
 class ServerError(RequestsError):
     message = "While handling the request something went wrong on the server side."
     def __init__(self, body=None):
-        super(BadRequestError, self).__init__(500, message, body)
+        super(ServerError, self).__init__(500, self.message, body)
+
+_errors = {
+        400: BadRequestError,
+        401: UnauthorizedError,
+        403: ForbiddenError,
+        404: NotFoundError,
+        405: MethodNotAllowedError,
+        409: ConflictError,
+        500: ServerError,
+        }
+
+def is_error(statuscode):
+    return statuscode in _errors
 
 def get_error_class(statuscode):
-    erorrs = {
-            400: BadRequestError,
-            401: UnauthorizedError,
-            403: ForbiddenError,
-            404: NotFoundError,
-            405: MethodNotAllowedError,
-            409: ConflictError,
-            500: ServerError,
-            }
     return errors[statuscode]
 
