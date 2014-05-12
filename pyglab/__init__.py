9f3289f45c727835c8f52b0c2489b06da2f03c25
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-12 13:54:24 +0200

    Make RequestError available in package root.

--- pyglab/__init__.py
+++ pyglab/__init__.py
@@ -5,4 +5,5 @@ __license__ = 'MIT License'
 __copyright__ = '(c) 2014 Michael Schlottke'
 
 from .pyglab import Pyglab
+from .exceptions import RequestError
 from .apirequest import ApiRequest, RequestType
