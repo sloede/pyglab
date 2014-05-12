9d45ec5b98601680f91f5be400857b9fb9a29220
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-12 14:50:15 +0200

    Ability to create/modify users.

--- pyglab/users.py
+++ pyglab/users.py
@@ -5,17 +5,54 @@ class Users(object):
         self._pyglab = pyglab
 
     def get(self, sudo=None, page=None, per_page=None):
-        u = self._pyglab.request(RequestType.GET, '/users', sudo, page,
+        r = self._pyglab.request(RequestType.GET, '/users', sudo, page,
                                  per_page)
         return u
 
     def by_id(self, uid, sudo=None, page=None, per_page=None):
-        u = self._pyglab.request(RequestType.GET, '/users/' + str(uid), sudo,
+        r = self._pyglab.request(RequestType.GET, '/users/' + str(uid), sudo,
                                  page, per_page)
         return u
 
     def by_name(self, name, sudo=None, page=None, per_page=None):
         params = {'search': name}
-        u = self._pyglab.request(RequestType.GET, '/users', params, sudo, page,
+        r = self._pyglab.request(RequestType.GET, '/users', params, sudo, page,
                                  per_page)
         return u
+
+    def create(self, email, password, username, name, sudo=None, page=None,
+               per_page=None, **kwargs):
+        params = {'email': email, 'password': password, 'username': username,
+                  'name': name}
+        params.update(kwargs)
+        r = self._pyglab.request(RequestType.POST, '/users', params, sudo, page,
+                                 per_page)
+        return u
+
+    def modify(self, uid, sudo=None, page=None, per_page=None, **kwargs):
+        params = kwargs
+        r = self._pyglab.request(RequestType.PUT, '/users/' + str(uid), params,
+                                 sudo, page, per_page)
+        return u
+
+class User(object):
+    def __init__(self, email, password, username, name,
+                 skype=None, linkedin=None, twitter=None, website_url=None,
+                 project_limits=None, extern_uid=None, provider=None, bio=None,
+                 admin=None, can_create_group=None):
+        self.data = {
+            'email': email,
+            'password': password,
+            'username': username,
+            'name': name,
+            'skype': skype,
+            'linkedin': linkedin,
+            'twitter': twitter,
+            'website_url': website_url,
+            'project_limits': project_limits,
+            'extern_uid': extern_uid,
+            'provider': provider,
+            'bio': bio,
+            'admin': admin,
+            'can_create_group': can_create_group
+        }
