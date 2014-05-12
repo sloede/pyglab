d9f2f528bc1c31992bba818a9c8317ca7fb2a80e
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-12 14:12:55 +0200

    Implement get, by_id, by_name.

--- pyglab/users.py
+++ pyglab/users.py
@@ -1,28 +1,21 @@
+from .apirequest import RequestType
+
 class Users(object):
-    def __init__(self, users):
-        self._users = users
+    def __init__(self, pyglab):
+        self._pyglab = pyglab
 
-    def __len__(self):
-        return len(self._users)
+    def get(self, sudo=None, page=None, per_page=None):
+        u = self._pyglab.request(RequestType.GET, '/users', sudo, page,
+                                 per_page)
+        return u
 
-    def __getitem__(self, key):
-        return self._users[key]
+    def by_id(self, uid, sudo=None, page=None, per_page=None):
+        u = self._pyglab.request(RequestType.GET, '/users/' + str(uid), sudo,
+                                 page, per_page)
+        return u
 
-    def __iter__(self):
-        return UsersIter(self)
-
-
-class UsersIter(object):
-    def __init__(self, users):
-        self.users = users
-        self.index = -1
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index < len(self.users) - 1:
-            self.index += 1
-            return self.users[self.index]
-        else:
-            raise StopIteration
+    def by_name(self, name, sudo=None, page=None, per_page=None):
+        params = {'search': name}
+        u = self._pyglab.request(RequestType.GET, '/users', params, sudo, page,
+                                 per_page)
+        return u
