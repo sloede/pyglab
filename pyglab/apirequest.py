48992da38558c7b93dcccc64a9964ebbd153d6db
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-16 18:30:02 +0200

    Add flexibility & convenience through persistence
    Also split init from request.

--- pyglab/apirequest.py
+++ pyglab/apirequest.py
@@ -16,8 +16,12 @@ class ApiRequest:
         RequestType.DELETE: requests.delete,
     }
 
-    def __init__(self, request_type, url, token, params={}, sudo=None,
-                 page=None, per_page=None):
+    def __init__(self, token, sudo=None):
+        self._r = None
+        self._token = token
+        self._sudo = sudo
+
+    def request(self, request_type, url, params={}, token=None, sudo=None):
         # Build header
         header = {'PRIVATE-TOKEN': token}
         if sudo is not None:
@@ -31,14 +35,16 @@ class ApiRequest:
 
         r = self._request_creators[request_type](url, params=params,
                                                  headers=header)
-        content = json.loads(r.text)
 
         if RequestError.is_error(r.status_code):
             raise RequestError.error_class(r.status_code)(content)
 
-        self._content = content
+        self._r = r
 
     @property
     def content(self):
-        return self._content
+        return json.loads(self._r.text)
 
+    @property
+    def links(self):
+        return self._r.links
