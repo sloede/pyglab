6f41ef55eeaafce449712555152a1368a9a6f776
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-16 18:32:25 +0200

    Add support for returning results as generator.

--- pyglab/pyglab.py
+++ pyglab/pyglab.py
@@ -30,12 +30,29 @@ class Pyglab(object):
         self._per_page = per_page
         return previous
 
-    def request(self, request_type, url, params={}, sudo=None):
+    def request(self, request_type, url, params={}, sudo=None, generator=False):
         if sudo is None and self._user is not None:
             sudo = _self.user
-        r = ApiRequest(request_type, self._base_url + '/' + url.lstrip('/'),
-                       self._token, params, sudo)
-        return r.content
+        r = ApiRequest(self._token, sudo)
+        full_url = self._base_url + '/' + url.lstrip('/')
+        if per_page not in params and self._per_page is not None:
+            params['per_page'] = self._per_page
+
+        if not generator:
+            r.request(request_type, full_url, params)
+            return r.content
+
+        queue = r.request(request_type, full_url, params)
+        last = False
+        while True:
+            if len(queue) == 0:
+                if last:
+                    break
+                if r.links['next']['url'] == r.links['last']['url']:
+                    last = True
+                r.request(request_type, r.links['next']['url'], params)
+                queue.extend(r.content)
+            yield queue.pop(0)
 
     @property
     def users(self):
