4d24a754a62db85f33a21a6698c17518306fac2d
Michael Schlottke <michael.schlottke@sloede.com>
2014-05-18 14:05:53 +0200

    Add first package docstring

--- pyglab/__init__.py
+++ pyglab/__init__.py
@@ -1,7 +1,17 @@
+"""
+Pyglab
+~~~~~~
+
+Pyglab is a wrapper for the GitLab API, written in Python.
+
+:copyright: (c) 2014 Michael Schlottke
+:license: MIT (see LICENSE for more details)
+"""
+
 __title__ = 'pyglab'
 __version__ = '0.2.0dev'
 __author__ = 'Michael Schlottke'
-__license__ = 'MIT License'
+__license__ = 'MIT'
 __copyright__ = '(c) 2014 Michael Schlottke'
 
 from .pyglab import Pyglab
